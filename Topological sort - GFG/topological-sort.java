4ba5baa1fbc0efebe9b072e04add8a2d6d19aca0
CHETHANA SREE R <107319671+CHETHANAR11@users.noreply.github.com>
2023-11-03 09:57:46 +0530

    Added solution - LeetHub

---
+++ Topological sort - GFG/topological-sort.java
@@ -0,0 +1,99 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+
+        while (t-- > 0) {
+            ArrayList<ArrayList<Integer>> list = new ArrayList<>();
+            String st[] = read.readLine().trim().split("\\s+");
+            int edg = Integer.parseInt(st[0]);
+            int nov = Integer.parseInt(st[1]);
+
+            for (int i = 0; i < nov; i++)
+                list.add(i, new ArrayList<Integer>());
+
+            int p = 0;
+            for (int i = 1; i <= edg; i++) {
+                String s[] = read.readLine().trim().split("\\s+");
+                int u = Integer.parseInt(s[0]);
+                int v = Integer.parseInt(s[1]);
+                list.get(u).add(v);
+            }
+
+            int[] res = new Solution().topoSort(nov, list);
+
+            if (check(list, nov, res) == true)
+                System.out.println("1");
+            else
+                System.out.println("0");
+        }
+    }
+    static boolean check(ArrayList<ArrayList<Integer>> list, int V, int[] res) {
+        
+        if(V!=res.length)
+        return false;
+        
+        int[] map = new int[V];
+        for (int i = 0; i < V; i++) {
+            map[res[i]] = i;
+        }
+        for (int i = 0; i < V; i++) {
+            for (int v : list.get(i)) {
+                if (map[i] > map[v]) return false;
+            }
+        }
+        return true;
+    }
+}
+
+// } Driver Code Ends
+
+
+/*Complete the function below*/
+
+
+
+class Solution {
+    static int vertices;
+
+    static int[] topoSort(int V, ArrayList<ArrayList<Integer>> adj) {
+        vertices = V;
+        int[] indegree = new int[vertices];
+
+        for (int u = 0; u < vertices; u++) {
+            for (int v : adj.get(u)) {
+                indegree[v]++;
+            }
+        }
+
+        Queue<Integer> q = new LinkedList<>();
+        int[] ans = new int[vertices];
+        int idx = 0;
+
+        for (int i = 0; i < vertices; i++) {
+            if (indegree[i] == 0) {
+                q.add(i);
+            }
+        }
+        while (!q.isEmpty()) {
+            int u = q.poll();
+
+            ans[idx] = u;
+            idx++;
+
+            for (int v : adj.get(u)) {
+                indegree[v]--;
+                if (indegree[v] == 0) {
+                    q.add(v);
+                }
+            }
+        }
+        return ans;
+    }
+}
